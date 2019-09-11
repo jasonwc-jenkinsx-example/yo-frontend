1e05e5052c0e75e19cdcfea04307f10bbaf51f8b
jasonwc <jasonwccodes@gmail.com>
2019-09-11 13:54:11 -0600

    Fix owners, update preview requirements to be more permissive, preflight some changes for demo

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import settings from './config.js'
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { responses: [] };
+    this.state = {
+      yo_responses: []
+    };
   }
 
-  onClickHandler = () => {
+  onYoClickHandler = () => {
     fetch(`${settings.API_URL}/api/v1/yo`)
       .then(res => res.json())
       .then(
         (result) => {
           this.setState(
-            { responses: this.state.responses.concat(result.message) }
+            { yo_responses: this.state.responses.concat(result.message) }
           );
         }
       );
@@ -26,8 +28,8 @@ class App extends React.Component {
         <h1>Yo Frontend</h1>
         <p> Connected to { settings.API_URL }</p>
         <div>
-          <button onClick={this.onClickHandler}>Say "Yo"</button>
-          { this.state.responses.map(response => (
+          <button onClick={this.onYoClickHandler}>Say "Yo"</button>
+          { this.state.yo_responses.map(response => (
             <p>{ response }</p>
           ))}
         </div>
