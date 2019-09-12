f0bbdd07f386728d596e95f0ef1e5cf4bcfd7561
jasonwc <jasonwccodes@gmail.com>
2019-09-12 12:45:09 -0600

    Specify 'application_url' in cypress environment.

--- cypress/integration/smoke_test_spec.js
+++ cypress/integration/smoke_test_spec.js
@@ -1,11 +1,11 @@
 describe('smoke tests', function() {
   it('has the correct header', function() {
-    cy.visit('http://localhost:3000/');
+    cy.visit(Cypress.env('application_url'));
     cy.contains('Yo Frontend');
   })
 
   it('it calls the backend and displays the result when the "say yo" button is clicked', function() {
-    cy.visit('http://localhost:3000/');
+    cy.visit(Cypress.env('application_url'));
     cy.contains('Say "Yo"').click();
     cy.contains("yo yo");
   })
