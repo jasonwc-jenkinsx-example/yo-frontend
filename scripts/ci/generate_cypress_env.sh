f79b856365fef9aedde56a2351418369f631074b
jasonwc <jasonwccodes@gmail.com>
2019-09-13 14:40:58 -0600

    Use correct namespace

--- scripts/ci/generate_cypress_env.sh
+++ scripts/ci/generate_cypress_env.sh
@@ -3,7 +3,7 @@
 # preview environment
 
 PREVIEW_NAMESPACE="$(echo $JOB_NAME | tr / - | tr '[:upper:]' '[:lower:]')"
-APPLICATION_URL="http://yo-frontend.${PREVIEW_NAMESPACE}.jenkins-x-example.dev.launch.adobe.net"
+APPLICATION_URL="http://yo-frontend.jx-${PREVIEW_NAMESPACE}.jenkins-x-example.dev.launch.adobe.net"
 
 jq -n \
   --arg application_url "$APPLICATION_URL" \
