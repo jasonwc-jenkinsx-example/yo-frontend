018a32c37c91bebc6158da64bbd6632dd926dab1
jasonwc <jasonwccodes@gmail.com>
2019-09-13 14:28:34 -0600

    Generate a cypress.env.json for the preview environment

---
+++ scripts/ci/generate_cypress_env.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+# Ouputs a cypress.env.json for generating the cypress configuration for a
+# preview environment
+
+PREVIEW_NAMESPACE="$(echo $JOB_NAME | tr / - | tr '[:upper:]' '[:lower:]')"
+APPLICATION_URL="http://yo-frontend.${PREVIEW_NAMESPACE}.jenkins-x-example.dev.launch.adobe.net"
+
+jq -n \
+  --arg application_url "$APPLICATION_URL" \
+  '{application_url: $application_url}' > cypress.env.json
